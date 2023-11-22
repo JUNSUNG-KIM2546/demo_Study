6391460ca2bce6a48cb2259a2978dd88a7b05a54
JunnnMo3o <kjs7953@gmail.com>
2023-11-22 21:39:42 +0900

    11.22 demo

--- demo/src/main/java/ssw/demo/study/Main24.java
+++ demo/src/main/java/ssw/demo/study/Main24.java
@@ -5,32 +5,34 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main24 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//인풋
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	//아웃풋
-		String t = "";
-		String[] list = new String[];
 		
-		while (true) {
-			t = br.readLine();
-			StringTokenizer st = new StringTokenizer(t, " ");
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			
-			if ((A==0)&&(B==0)) {
-				bw.close(); //종료
-				break;
-			}
-			else {
-				int ab =  A+B;	// 스트링을 인트로 변환(계산)
-				bw.write(Integer.toString(ab));	// 버퍼는 스트링으로 받아서 스트링으로 출력 해야한다
-				bw.newLine();	// 개행 줄 추가
-			}
-			bw.flush(); //출력
+		int count = 0;
+		int i = Integer.parseInt(br.readLine());
+		int[] list = new int[i];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int j=0; j<list.length; j++) {
+			list[j] = Integer.parseInt(st.nextToken());
 		}
+		int c = Integer.parseInt(br.readLine());
+		for(int ce : list) {
+			if(c == ce) {
+				count++;
+			}
+		}
+		
+		bw.write(Integer.toString(count));	// 버퍼는 스트링으로 받아서 스트링으로 출력 해야한다
+		bw.newLine();	// 개행 줄 추가
+		bw.flush(); //출력
+		br.close(); //종료
+		bw.close(); //종료
 		
 	}
 }
@@ -43,6 +45,9 @@ public class Main24 {
 		//bw.newLine(); // 줄바꿈
 		//bw.flush(); //출력
 		//bw.close(); //종료
+		//String li = Arrays.toString(list);	// 정수형 배열을 문자열 배열로 변환
+		//int B = Integer.parseInt(st.nextToken());
+		//list[j] = Integer.parseInt(st.nextToken());
 */
 
 		/*
---
+++ demo/src/main/java/ssw/demo/study/Main26.java
@@ -0,0 +1,60 @@
+package ssw.demo.study;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main26 {
+	 public static void main(String[] args) throws Exception {
+
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    int ma = -1000000;
+	    int mi = 1000000;
+	    
+	    
+	    int N = Integer.parseInt(br.readLine()); // 입력받는 배열의 크기
+	    int[] list = new int[N];
+	    
+	    String input = br.readLine(); // 배열에 입력될 값
+	    StringTokenizer st = new StringTokenizer(input, " ");
+	    
+	    for(int i=0; i<list.length; i++) {
+	        list[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    for(int tot : list) {
+    		ma = Math.max(tot, ma);
+    		mi = Math.min(tot, mi);
+	    }
+	    bw.write(Integer.toString(mi));
+		bw.write(" ");
+		bw.write(Integer.toString(ma));
+	    bw.flush();
+	    bw.close();
+	    br.close();
+	}
+}
+
+/*
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		//String s = br.readLine();
+		//bw.write(co); // 할당
+		//bw.newLine(); // 줄바꿈
+		//bw.flush(); //출력
+		//bw.close(); //종료
+		//String li = Arrays.toString(list);	// 정수형 배열을 문자열 배열로 변환
+		//int B = Integer.parseInt(st.nextToken());
+		//list[j] = Integer.parseInt(st.nextToken());
+*/
+
+		/*
+		|\_/|
+		|q p|   /}
+		( 0 )"""\
+		|"^"`    |
+		||_/=\\__|
+		*/
