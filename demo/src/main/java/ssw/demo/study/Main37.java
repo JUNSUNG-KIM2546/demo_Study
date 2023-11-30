9299bd6ffcf7179eef80d55414291306e091d4f5
JunnnMo3o <kjs7953@gmail.com>
2023-11-30 21:58:41 +0900

    11.30 @JunnMo3o

--- demo/src/main/java/ssw/demo/study/Main37.java
+++ demo/src/main/java/ssw/demo/study/Main37.java
@@ -5,23 +5,18 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main36 {
+public class Main37 {
 	 public static void main(String[] args) throws Exception {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	    
-	    int co = Integer.parseInt(br.readLine());
-	    
-	    for(int i=0; i<co; i++) {
-	    	String input = br.readLine();
-	    	bw.write(input.charAt(0));
-	    	bw.write(input.charAt(input.length()-1));
-	    	bw.newLine();
-	    }
-	    
-	    bw.flush();
-	    bw.close();
-	    br.close();
+		String input = br.readLine();
+			    
+	    	bw.write(Integer.toString(input.charAt(0)));
+		    
+		    bw.flush();
+		    bw.close();
+		    br.close();
 	}
 }
 /*
